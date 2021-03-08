89481be020651db127316a647d484ac886c97672
Showmick Kar <showmick12@gmail.com>
2021-03-08 19:53:40 +0600

    Create  Lift .cpp

---
+++ Lift .cpp
@@ -0,0 +1,42 @@
+/* Problem Link: https://lightoj.com/problem/lift */
+// Problem Type: Adhoc
+
+
+#include <bits/stdc++.h>
+#define INF 1e18
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+#define arr array
+#define endl "\n";
+#define _min(a) *min_element(a.begin(), a.end());
+#define _max(a) *max_element(a.begin(), a.end());
+#define _sort(x) sort(x.begin(), x.end())
+#define _rsort(x) sort(x.rbegin(), x.rend())
+#define _reverse(x) reverse(x.begin(), x.end())
+#define _debug(x) cout << #x << " = " << x << endl;
+#define debug(x, y) cout << #x " = " << x << " " << #y << " = " << y << endl;
+#define _lower_bound(a, x) lower_bound(a.begin(), a.end(), x) - a.begin()
+#define _upper_bound(a, x) upper_bound(a.begin(), a.end(), x) - a.begin()
+typedef long long ll;
+using namespace std;
+
+void solve() {	
+	int pos, lift;
+	int ans = 16;
+	cin >> pos >> lift;
+	ans += abs(pos - lift) * 4 + pos * 4; 
+	cout << ans + 3 << endl;
+}   
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int t, i = 1; cin >> t;
+    while(t--) {
+    	cout << "Case " << i << ": ";
+        solve();
+        ++i;
+    }
+}  
